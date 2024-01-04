e10398b3ef55a1111ca3720abd82fa6fc5b4849a
Sarabjit Dhiman <isarabjitdhiman@gmail.com>
2024-01-04 18:14:09 +0530

    Update request features
    Update generate_features function in util.py module.

--- tweeterpy/util.py
+++ tweeterpy/util.py
@@ -57,17 +57,17 @@ def generate_features(default_features=True, user_data_features=False, user_info
         features.update(default_features)
 
     if user_data_features or user_info_feautres:
-        features.update({"hidden_profile_likes_enabled": False, "highlights_tweets_tab_ui_enabled": True,
-                         "creator_subscriptions_tweet_preview_api_enabled": True})
+        features.update({"hidden_profile_likes_enabled": False, "hidden_profile_subscriptions_enabled":True, "highlights_tweets_tab_ui_enabled": True,
+                         "responsive_web_twitter_article_notes_tab_enabled":False, "creator_subscriptions_tweet_preview_api_enabled": True})
         if user_info_feautres:
             features.update(
-                {"subscriptions_verification_info_verified_since_enabled": True})
+                {"subscriptions_verification_info_is_identity_verified_enabled":True, "subscriptions_verification_info_verified_since_enabled": True})
 
     if additional_features:
-        features.update({"rweb_lists_timeline_redesign_enabled": True, "creator_subscriptions_tweet_preview_api_enabled": True, "tweetypie_unmention_optimization_enabled": True,
+        features.update({"rweb_lists_timeline_redesign_enabled": True, "creator_subscriptions_tweet_preview_api_enabled": True, "c9s_tweet_anatomy_moderator_badge_enabled":True,"tweetypie_unmention_optimization_enabled": True,
                          "responsive_web_edit_tweet_api_enabled": True, "graphql_is_translatable_rweb_tweet_is_translatable_enabled": True, "view_counts_everywhere_api_enabled": True,
                          "longform_notetweets_consumption_enabled": True, "responsive_web_twitter_article_tweet_consumption_enabled": False, "tweet_awards_web_tipping_enabled": False,
-                         "freedom_of_speech_not_reach_fetch_enabled": True, "standardized_nudges_misinfo": True, "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": False,
+                         "freedom_of_speech_not_reach_fetch_enabled": True, "standardized_nudges_misinfo": True, "tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled": False, "rweb_video_timestamps_enabled":True,
                          "longform_notetweets_rich_text_read_enabled": True, "longform_notetweets_inline_media_enabled": False, "responsive_web_media_download_video_enabled": False, "responsive_web_enhance_cards_enabled": False})
 
     return features
