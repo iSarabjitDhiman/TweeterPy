c958734b793188bd1d766cfe6725ced0ddfceb1d
Sarabjit Dhiman <isarabjitdhiman@gmail.com>
2026-02-24 21:50:21 +0530

    Add core/session.py module

---
+++ tweeterpy/core/session.py
@@ -0,0 +1,44 @@
+from typing import Any, Dict, Union
+
+import curl_cffi
+
+from tweeterpy.core.abstractions import HttpMethod, TweeterPySession
+
+
+class BaseSession(TweeterPySession):
+    def __init__(self) -> None:
+        super().__init__()
+        self._session: Union[curl_cffi.Session, curl_cffi.AsyncSession]
+
+    def update_headers(self, headers: Dict[str, Any]):
+        self._session.headers.update(headers=headers)
+
+    @property
+    def headers(self):
+        return self._session.headers
+
+    @property
+    def cookies(self):
+        return self._session.cookies
+
+
+class Session(BaseSession):
+    def __init__(self) -> None:
+        super().__init__()
+        self._session = curl_cffi.Session(impersonate="chrome")
+
+    def _send(self, url: str, method: HttpMethod, **kwargs):
+        return self._session.request(url=url, method=method, **kwargs)
+
+
+class AsyncSession(TweeterPySession):
+    def __init__(self) -> None:
+        super().__init__()
+        self._session = curl_cffi.AsyncSession(impersonate="chrome")
+
+    async def _send(self, url: str, method: HttpMethod, **kwargs):
+        return await self._session.request(url=url, method=method, **kwargs)
+
+
+if __name__ == "__main__":
+    pass
