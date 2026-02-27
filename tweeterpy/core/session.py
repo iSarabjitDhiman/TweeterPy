2a16b8259b380bccd6704c3ccf80068112c021b0
Sarabjit Dhiman <isarabjitdhiman@gmail.com>
2026-02-27 18:49:07 +0530

    Update BaseSession to add default request and response handlers

--- tweeterpy/core/session.py
+++ tweeterpy/core/session.py
@@ -1,17 +1,30 @@
-from typing import Any, Dict, Union
+from typing import Any, Dict
 
 import curl_cffi
 
+from tweeterpy.config import TweeterPyConfig
 from tweeterpy.core.abstractions import HttpMethod, TweeterPySession
+from tweeterpy.core.handlers import RequestHandlers, ResponseHandlers
 
 
 class BaseSession(TweeterPySession):
-    def __init__(self) -> None:
+    def __init__(self, session) -> None:
         super().__init__()
-        self._session: Union[curl_cffi.Session, curl_cffi.AsyncSession]
+
+        self._session: Any = session
+
+        self.request_hooks.extend([RequestHandlers.inject_auth_headers,
+                                  RequestHandlers.inject_guest_token,
+                                  RequestHandlers.inject_twitter_headers,
+                                  RequestHandlers.inject_transaction_id])
+
+        self.response_hooks.extend(
+            [ResponseHandlers.twitter_cookie_injector_hook, ResponseHandlers.twitter_guest_token_handler])
+
+        self.update_headers(TweeterPyConfig.headers())
 
     def update_headers(self, headers: Dict[str, Any]):
-        self._session.headers.update(headers=headers)
+        self._session.headers.update(headers)
 
     @property
     def headers(self):
@@ -24,17 +37,17 @@ class BaseSession(TweeterPySession):
 
 class Session(BaseSession):
     def __init__(self) -> None:
-        super().__init__()
-        self._session = curl_cffi.Session(impersonate="chrome")
+        session = curl_cffi.Session(impersonate="chrome")
+        super().__init__(session=session)
 
     def _send(self, url: str, method: HttpMethod, **kwargs):
         return self._session.request(url=url, method=method, **kwargs)
 
 
-class AsyncSession(TweeterPySession):
+class AsyncSession(BaseSession):
     def __init__(self) -> None:
-        super().__init__()
-        self._session = curl_cffi.AsyncSession(impersonate="chrome")
+        session = curl_cffi.AsyncSession(impersonate="chrome")
+        super().__init__(session=session)
 
     async def _send(self, url: str, method: HttpMethod, **kwargs):
         return await self._session.request(url=url, method=method, **kwargs)
